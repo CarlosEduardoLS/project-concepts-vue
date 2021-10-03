2be1dbb8116f8a3c043fee57885636d3941afac8
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-03 18:43:26 -0300

    First Commit

---
+++ Data-binding.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 :style="{ textdecoration: decoration }">Hello {{ name }}</h1>
+    <input type="text" v-model="name" />
+    <br />
+
+    <a :href="link">Link pro curso!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Carlos',
+    link: 'https://treinamento.vuejsbrasil.org',
+    decoration: 'underline',
+  }),
+}
+</script>
+
+<style></style>
